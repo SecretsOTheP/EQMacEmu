4c49b0640758a6b68bc9e5cf176d6b7af37ac3df
jududdar <BryantBW@gmail.com>
2024-09-25 00:33:48 -0400

    Ungate Wax Flower and Nodding Blue Lily

---
+++ utils/sql/git/content/2024_09_25_Ungate_WaxFlower_NoddingBlueLily.sql
@@ -0,0 +1,3 @@
+/* Ungate Wax Flower and Nodding Blue Lily drops */
+UPDATE lootdrop_entries SET min_expansion = 1 WHERE item_id = 24093; -- Wax Flower
+UPDATE lootdrop_entries SET min_expansion = 1 WHERE item_id = 24094; -- Nodding Blue Lily
