f1ea80b54550bdb9fd15151bae36bb8c526d1c55
CoastalRedwood <172213097+CoastalRedwood@users.noreply.github.com>
2025-03-21 17:12:47 -0700

    Modify #mystats to allow limited info for players
    - Allow AccountStatus::Player access to #mystats
    - AccountStatus < Guide is routed to a new
      SendStatsToPlayer() with limited information
      exposing server calculated values for things
      like capped mitigation, attack, haste, etc

--- zone/gm_commands/mystats.cpp
+++ zone/gm_commands/mystats.cpp
@@ -1,7 +1,9 @@
 #include "../client.h"
 
 void command_mystats(Client *c, const Seperator *sep){
-	if (c->GetTarget() && c->GetPet()) {
+	if (c->Admin() < AccountStatus::Guide)
+		c->SendStatsToPlayer(c);
+	else if (c->GetTarget() && c->GetPet()) {
 		if (c->GetTarget()->IsPet() && c->GetTarget() == c->GetPet())
 			c->GetTarget()->ShowStats(c);
 		else
