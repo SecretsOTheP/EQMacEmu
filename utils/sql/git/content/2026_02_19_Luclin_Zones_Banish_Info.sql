cf64c1cb32385046497cfc53015a0e6a27f75740
Brandon Way <brandon.way@gmail.com>
2026-03-12 06:14:39 -0700

    Lockout Banish Grace Period (#350)
    * Lockout grace period + Luclin zone banish info
    
    1. Add Rule for LockoutGracePeriod (in seconds).
    2. Add check to not punt from zone if the time elapsed since lockout is within the grace period.
    3. Add an SQL patch to set banish zone information on Luclin raid zones so lockout punts are not sent to bind.
    
    * SQL Match zoneidnumber instead of id
    
    Match zoneidnumber instead of id for SQL patch.
    
    * Minor cleanup
    
    Update comment and current_time variable name.

---
+++ utils/sql/git/content/2026_02_19_Luclin_Zones_Banish_Info.sql
@@ -0,0 +1,19 @@
+-- Set Banish Zone info for Luclin raid zones
+-- SSeru -> Dawnshroud
+UPDATE zone SET banish_zone_id = 174, banish_x = 2080, banish_y = 0, banish_z = 89.31 WHERE zoneidnumber = 159 LIMIT 1;
+-- VT -> Umbral
+UPDATE zone SET banish_zone_id = 176, banish_x = 1935, banish_y = -475, banish_z = 23.06 WHERE zoneidnumber = 158 LIMIT 1;
+-- Umbral -> ME
+UPDATE zone SET banish_zone_id = 173, banish_x = -2000, banish_y = -220, banish_z = -153.28 WHERE zoneidnumber = 176 LIMIT 1;
+-- Griegs -> DSP
+UPDATE zone SET banish_zone_id = 174, banish_x = -860, banish_y = -1870, banish_z = 102.53 WHERE zoneidnumber = 163 LIMIT 1;
+-- Ssra -> Grey
+UPDATE zone SET banish_zone_id = 171, banish_x = 290, banish_y = -1845, banish_z = -20.09 WHERE zoneidnumber = 162 LIMIT 1;
+-- Akheva -> ME
+UPDATE zone SET banish_zone_id = 173, banish_x = 261, banish_y = -2290, banish_z = -156.87 WHERE zoneidnumber = 179 LIMIT 1;
+-- Acrylia -> Grimling
+UPDATE zone SET banish_zone_id = 167, banish_x = 1910, banish_y = 877, banish_z = -268.69 WHERE zoneidnumber = 154 LIMIT 1;
+-- Deep -> Echo
+UPDATE zone SET banish_zone_id = 153, banish_x = 1582, banish_y = -1646, banish_z = 125.13 WHERE zoneidnumber = 164 LIMIT 1;
+-- Katta -> Tenebrous
+UPDATE zone SET banish_zone_id = 172, banish_x = 1810, banish_y = -60, banish_z = -36.62 WHERE zoneidnumber = 160 LIMIT 1;
