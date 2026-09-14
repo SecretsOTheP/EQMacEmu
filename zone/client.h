efa067b07503fe4c074d9500aa25ef69c1a72922
kyinen <178090649+kyinen@users.noreply.github.com>
2026-09-14 12:16:46 -0500

    Set Rallosian Glory anti-abuse timers to fifteen minutes

--- zone/client.h
+++ zone/client.h
@@ -711,10 +711,10 @@ public:
 	static constexpr uint8 RallosianGloryZoneXPBonus = 5;
 	static constexpr float RallosianGloryRankXPBonus = 2.5f;
 	static constexpr float RallosianGloryRankAAXPBonus = 0.5f;
-	static constexpr uint32 RallosianGloryCooldownSeconds = 3600;
-	static constexpr uint32 RallosianGloryVictimCooldownSeconds = 3600;
-	static constexpr uint32 RallosianGloryKillerCooldownSeconds = 600;
-	static constexpr uint32 RallosianGloryMinimumZoneSeconds = 100;
+	static constexpr uint32 RallosianGloryCooldownSeconds = 900;
+	static constexpr uint32 RallosianGloryVictimCooldownSeconds = 900;
+	static constexpr uint32 RallosianGloryKillerCooldownSeconds = 900;
+	static constexpr uint32 RallosianGloryMinimumZoneSeconds = 125;
 	inline int32 ForumID() const { return forum_id; }
 	inline uint8 GetRallosianGlory() const { return rallosian_glory; }
 	inline void SetRallosianGlory(uint8 rank) { rallosian_glory = rank > RallosianGloryMaxRank ? RallosianGloryMaxRank : rank; }
