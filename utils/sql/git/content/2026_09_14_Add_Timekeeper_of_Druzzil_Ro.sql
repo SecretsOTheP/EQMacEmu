0e161c1c6af539b07b418ee382ae0b1d5abce505
kyinen <178090649+kyinen@users.noreply.github.com>
2026-09-14 14:16:14 -0500

    Add Timekeeper of Druzzil Ro placement

--- utils/sql/git/content/2026_09_14_Add_Timekeeper_of_Druzzil_Ro.sql
+++ utils/sql/git/content/2026_09_14_Add_Timekeeper_of_Druzzil_Ro.sql
@@ -1,5 +1,5 @@
 -- Timekeeper of Druzzil Ro in Plane of Tranquility.
--- In-game X -2232.19, Y 1170.21, Z -899.16.
+-- In-game location: X -2232.19, Y 1170.21, Z -899.16.
 
 UPDATE npc_types AS timekeeper
 JOIN npc_types AS agent ON agent.name = 'Agent_of_Druzzil_Ro'
@@ -18,36 +18,19 @@ SET timekeeper.name = 'Timekeeper_of_Druzzil_Ro',
     timekeeper.luclin_beard = agent.luclin_beard
 WHERE timekeeper.id = 7151002;
 
-INSERT INTO spawnentry
-    (spawngroupID, npcID, chance, mintime, maxtime, min_expansion, max_expansion, content_flags, content_flags_disabled)
+INSERT INTO spawnentry (spawngroupID, npcID, chance, mintime, maxtime, min_expansion, max_expansion, content_flags, content_flags_disabled)
 SELECT 7151002, 7151002, 100, 0, 0, -1, -1, NULL, NULL
 WHERE NOT EXISTS (
-    SELECT 1 FROM spawnentry
-    WHERE spawngroupID = 7151002 AND npcID = 7151002
+    SELECT 1 FROM spawnentry WHERE spawngroupID = 7151002 AND npcID = 7151002
 );
 
 UPDATE spawn2
-SET x = -2232.19,
-    y = 1170.21,
-    z = -899.16,
-    heading = 0,
-    respawntime = 3600,
-    variance = 0,
-    enabled = 1
-WHERE zone = 'potranquility'
-  AND spawngroupID = 7151002;
+SET x = -2232.19, y = 1170.21, z = -899.16, heading = 0,
+    respawntime = 3600, variance = 0, enabled = 1
+WHERE zone = 'potranquility' AND spawngroupID = 7151002;
 
-INSERT INTO spawn2
-    (id, spawngroupID, zone, x, y, z, heading, respawntime, variance,
-     pathgrid, _condition, cond_value, enabled, animation,
-     boot_respawntime, clear_timer_onboot, boot_variance, force_z,
-     min_expansion, max_expansion, content_flags, content_flags_disabled)
-SELECT
-    (SELECT COALESCE(MAX(id), 0) + 1 FROM spawn2),
-    7151002, 'potranquility', -2232.19, 1170.21, -899.16, 0, 3600, 0,
-    0, 0, 1, 1, 0, 0, 0, 0, 0, -1, -1, NULL, NULL
+INSERT INTO spawn2 (id, spawngroupID, zone, x, y, z, heading, respawntime, variance, pathgrid, _condition, cond_value, enabled, animation, boot_respawntime, clear_timer_onboot, boot_variance, force_z, min_expansion, max_expansion, content_flags, content_flags_disabled)
+SELECT (SELECT COALESCE(MAX(id), 0) + 1 FROM spawn2), 7151002, 'potranquility', -2232.19, 1170.21, -899.16, 0, 3600, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, -1, -1, NULL, NULL
 WHERE NOT EXISTS (
-    SELECT 1 FROM spawn2
-    WHERE zone = 'potranquility'
-      AND spawngroupID = 7151002
+    SELECT 1 FROM spawn2 WHERE zone = 'potranquility' AND spawngroupID = 7151002
 );
